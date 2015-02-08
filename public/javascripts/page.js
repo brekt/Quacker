661b687bd5560ee61b29ab687a4ff3783bf709d4
MattCordeiro <matthew3cord@gmail.com>
2015-02-08 00:48:27 -0800

    Refactored drop down js to accept elements.

--- public/javascripts/page.js
+++ public/javascripts/page.js
@@ -1,15 +1,35 @@
 $(document).ready(function(){
   console.log('Ready!');
-  $('#signin').mouseenter(function(ee){
-    ee.preventDefault();
-    $('#signinform').slideDown('slow', function(){
-      //animation complete
+  //$('#signin').mouseenter(function(ee){
+  //  ee.preventDefault();
+  //  $('#signinform').slideDown('slow', function(){
+  //    //animation complete
+  //  });
+  //});
+  //$('#signinform').mouseleave(function(ee){
+  //    ee.preventDefault();
+  //    $(this).slideUp('slow', function(){
+  //    //animation complete
+  //  });
+  //});
+
+function dropdown(link, form){
+    $(link).mouseenter(function(ee){
+        ee.preventDefault();
+        $(form).slideDown('slow', function(){
+            //animation complete
+        });
     });
-  });
-  $('#signinform').mouseleave(function(ee){
-      ee.preventDefault();
-      $(this).slideUp('slow', function(){
-      //animation complete
+    $(form).mouseleave(function(ee){
+        ee.preventDefault();
+       $(this).slideUp('slow', function(){
+           //animation complete
+       });
     });
-  });
+}
+dropdown('#signin', '#signinform');
+dropdown('#signup', '#signupform');
+
+
+
 });
