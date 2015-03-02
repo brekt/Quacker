cf5568d48eae74fd1ff5d92c0dd4fb1ca32ccaa7
mikeatgl <speakoftrees@gmail.com>
2015-03-02 15:49:44 -0800

    Now able to add uers to database.

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.get('/', function(req, res, next) {
   // use knex to pull up newest quacks
 
     knex.select('quack_content', 'quack_timestamp').from('quacks').orderBy('quack_timestamp', 'desc').limit(10).then(function(recentQuacks) {
-        console.log(recentQuacks);
+        // console.log(recentQuacks);
         res.render('index', { title: 'Quacker', recentQuacks: recentQuacks });
     });
   }
@@ -36,7 +36,6 @@ router.post('/', function(req, res, next){
   knex.transaction(function(trx){
     knex('quacks').transacting(trx).insert({quack_user_id: 2, quack_content: quack})
     .then(trx.commit)
-    .then(trx.rollback);
   }).then(function(){
     res.redirect('/');
   });
@@ -44,11 +43,16 @@ router.post('/', function(req, res, next){
 
 router.post('/signin', function(req, res, next) {
   res.cookie('registered', req.body.username);
-  res.redirect('/');
-  // knex.transaction(function(trx){
-  //   knex('users')
-  })
-})
+  var username = req.body.username;
+  var email = req.body.email;
+  var password = req.body.password;
+  knex.transaction(function(trx){
+    knex('users').transacting(trx).insert({user_name: username, user_email: email, user_password: password})
+    .then(trx.commit)
+  }).then(function(){
+    res.redirect('/');
+  });
+});
 
 
 
