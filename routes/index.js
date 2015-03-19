d61f031b1f54e77aad4c2919a1dd766599664abe
mikeatgl <speakoftrees@gmail.com>
2015-03-19 14:44:51 -0700

    Now with usernames.

--- routes/index.js
+++ routes/index.js
@@ -15,9 +15,12 @@ router.get('/', function(req, res, next) {
 
   // use knex to pull up newest quacks
 
-    knex.select('quack_content', 'quack_timestamp').from('quacks').orderBy('quack_timestamp', 'desc').limit(10).then(function(recentQuacks) {
-        // console.log(recentQuacks);
-        res.render('index', { title: 'Quacker', recentQuacks: recentQuacks });
+    // knex.select('quack_content', 'quack_timestamp', 'quack_user_id').from('quacks').orderBy('quack_timestamp', 'desc').limit(10).then(function(recentQuacks) {
+      knex('users')
+        .join('quacks', 'users.user_id', 'quacks.quack_user_id')
+        .select('users.user_name', 'quacks.quack_timestamp', 'quacks.quack_content').orderBy('quacks.quack_timestamp', 'desc').limit(10).then(function(recentQuacks) {
+          console.log(recentQuacks);
+          res.render('index', { title: 'Quacker', recentQuacks: recentQuacks });
     });
   }
   else {
