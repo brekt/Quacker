b7c119e2331f314cae78db72a06e769e6c626ba5
mikeatgl <speakoftrees@gmail.com>
2015-04-09 13:40:55 -0700

    Set master branch DB settings back to local instead of Heroku.

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,10 @@ module.exports = {
 		debug: true,
 		client: 'postgres',
 		connection: {
-		host     : process.env.APP_DB_HOST     || 'ec2-107-20-244-39.compute-1.amazonaws.com',
-		user     : process.env.APP_DB_USER     || 'rbomcnbjktpcin',
-		password : process.env.APP_DB_PASSWORD || 'cUB7nDq4WEtXXiy6YJDDH-Dw1M',
-		database : process.env.APP_DB_NAME     || 'd33sf6u0qfn8ru'
+		host     : process.env.APP_DB_HOST     || '127.0.0.1',
+		user     : process.env.APP_DB_USER     || '',
+		password : process.env.APP_DB_PASSWORD || '',
+		database : process.env.APP_DB_NAME     || 'quacker'
 		}
 	}
 };
