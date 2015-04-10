9007f9794124867a4853da48ff9b9b008a09f37e
mikeatgl <speakoftrees@gmail.com>
2015-04-09 19:40:47 -0700

    Updating Heroku/PG setup.

--- knexfile.js
+++ knexfile.js
@@ -4,9 +4,10 @@ module.exports = {
 		client: 'postgres',
 		connection: {
 		host     : process.env.APP_DB_HOST     || 'ec2-107-20-244-39.compute-1.amazonaws.com',
-		user     : process.env.APP_DB_USER     || 'rbomcnbjktpcin',
-		password : process.env.APP_DB_PASSWORD || 'cUB7nDq4WEtXXiy6YJDDH-Dw1M',
+		user     : process.env.APP_DB_USER     || 'ijrwrjdbcumfku',
+		password : process.env.APP_DB_PASSWORD || 'TbyGyc18xfbAfWrTxz1_8Q2wJX',
 		database : process.env.APP_DB_NAME     || 'd33sf6u0qfn8ru'
 		}
 	}
 };
+
